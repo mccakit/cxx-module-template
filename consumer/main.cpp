59245a29b15d04b01b1b2745a7a5687cebfa253b
mccakit <mccakit09@gmail.com>
2025-12-13 12:10:22 +0300

    one package multiple modules

--- consumer/main.cpp
+++ consumer/main.cpp
@@ -1,10 +1,13 @@
 import std;
 import math;
+import math2;
 import copy;
 
 int main()
 {
     std::cout << math::add(1, 2) << std::endl;
+    std::cout << math2::add(2.0, 3.0) << std::endl;
+
     char src[]  = "hello";
     char dest[6]{};
 
---
+++ package1/math2.cppm
@@ -0,0 +1,9 @@
+export module math2;
+import std;
+export namespace math2
+{
+int add(float a, float b)
+{
+    return a + b;
+}
+} // namespace math2
