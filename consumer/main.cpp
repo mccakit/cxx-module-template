8ae85981f0857dc4a630d7aa449dcfbfb03d1550
mccakit <mccakit09@gmail.com>
2025-12-13 12:01:09 +0300

    multiple packages

--- consumer/main.cpp
+++ consumer/main.cpp
@@ -1,7 +1,14 @@
-import math;
 import std;
+import math;
+import copy;
 
 int main()
 {
     std::cout << math::add(1, 2) << std::endl;
+    char src[]  = "hello";
+    char dest[6]{};
+
+    copy::copy(src, dest, 6);
+
+    std::cout << dest << '\n';
 }
